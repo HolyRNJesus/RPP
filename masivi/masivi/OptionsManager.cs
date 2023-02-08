541836b680c6342433083ebf1feb9131febce2b6
HolyRNJesus <veskotodorov786@protonmail.com>
2023-02-08 09:09:09 +0200

    zad1 solution

---
+++ masivi/masivi/OptionsManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masivi
+{
+    class OptionsManager
+    {
+        public static int CountLess100(int[] arr)
+        {
+            return arr.Count(x => x < 100);
+        }
+        public static int MaxNum(int[] arr)
+        {
+            return arr.Max();
+        }
+        public static int[] ReverseArray(int[] arr)
+        {
+            return arr.Reverse().ToArray();
+        }
+        public static void PrintArray(int[] arr)
+        {
+            Console.WriteLine(string.Join(", ", arr));
+        }
+        public static int EvenNumCount(int[] arr)
+        {
+            return arr.Count(x => x % 2 == 0);
+        }
+        public static int EvenNumSum(int[] arr)
+        {
+            arr = arr.Where(x => x % 2 == 0).ToArray();
+            return arr.Sum();
+        }
+        public static int[] EvenNum(int[] arr)
+        {
+            return arr.Where(x => x % 2 == 0).ToArray();
+        }
+    }
+}
---
+++ masivi/masivi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace masivi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int[] arr = new int[num];
+            for (int i = 0; i < num; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(OptionsManager.CountLess100(arr));
+            Console.WriteLine(OptionsManager.MaxNum(arr));
+            OptionsManager.PrintArray(OptionsManager.EvenNum(arr));
+            Console.WriteLine(OptionsManager.EvenNumSum(arr));
+            Console.WriteLine(OptionsManager.EvenNumCount(arr));
+            OptionsManager.PrintArray(OptionsManager.ReverseArray(arr));
+        }
+    }
+}
