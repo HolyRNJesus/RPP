9464a359c8fe404baca6d8bf81a0a2d51cda8773
rnjesus <veskotodorov786@proton.me>
2023-02-12 23:45:26 +0200

    optimizations

--- masivi2/masivi2/Program.cs
+++ masivi2/masivi2/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace masivi2
 {
@@ -7,14 +9,19 @@ namespace masivi2
     {
         static void Main(string[] args)
         {
-            
-            string[] arr = Console.ReadLine()!.Split().ToArray();
+            string[] arr = Console.ReadLine()!.Split();
 
             args = Console.ReadLine()!.Split();
             while (args[0] != "END") {
                 switch (args[0]) {
                     case "Reverse": {
-                        arr = arr.Reverse().ToArray();
+                        int left = 0;
+                        int right = arr.Length - 1;
+                        while (left < right) {
+                            (arr[left], arr[right]) = (arr[right], arr[left]); //swap
+                            left++;
+                            right--;
+                        }
                         break;
                     }
                     case "Distinct": {
@@ -32,7 +39,11 @@ namespace masivi2
                         break;
                     }
                     case "Print": {
-                        Console.WriteLine(string.Join(" ", arr.Where(x => x.StartsWith(args[1]))));
+                        StringBuilder sb = new();
+                        foreach (var item in arr.Where(x => x.StartsWith(args[1]))) {
+                            sb.Append(item + " ");
+                        }
+                        Console.WriteLine(sb.ToString());
                         break;
                     }
                 }
